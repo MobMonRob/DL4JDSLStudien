5cc2b011e4bcef654468d55eda153e12620f4e3c
Sebastian Bernauer <bernauerse@web.de>
2019-02-01 12:35:09 +0100

    Added support for global defined functions. Added tests for them. Added global function amountTimeElements.

--- PrePro/src/main/java/de/sbernauer/prepro/nodes/function/GlobalFunctions.java
+++ PrePro/src/main/java/de/sbernauer/prepro/nodes/function/GlobalFunctions.java
@@ -1,16 +1,38 @@
 package de.sbernauer.prepro.nodes.function;
 
 import de.sbernauer.prepro.nodes.FunctionTable;
+import de.sbernauer.prepro.variables.Constant;
 import de.sbernauer.prepro.variables.Variable;
+import org.nd4j.linalg.factory.Nd4j;
 
 import java.util.function.BiFunction;
 
 public abstract class GlobalFunctions {
 
     public static void registerGlobalFunctions(FunctionTable functionTable) {
-        functionTable.addFunction(createFunction("exists", (arguments, functionTable1) -> {
-            throw new RuntimeException();
+
+
+        // Returns first value
+        functionTable.addFunction(createFunction("testGlobalFunctionReturnFirstArgument", (arguments, ft) -> {
+            return arguments.getValues()[0];
         }));
+
+
+        //Returns second value
+        functionTable.addFunction(createFunction("testGlobalFunctionReturnSecondArgument", (arguments, ft) -> {
+            return arguments.getValues()[1];
+        }));
+
+
+        //Returns amount of time-elements for a given variable (must not be constant)
+        functionTable.addFunction(createFunction("amountTimeElements", (arguments, ft) -> {
+            if(arguments.getValues().length != 1 || arguments.getValues()[0] instanceof Constant) {
+                throw new RuntimeException("The function amountTimeElements must be called with one argument (not a constant).");
+            }
+            return new Constant(Nd4j.create(new double[]{arguments.getValues()[0].getAmountTimeElements()}, new int[]{1, 1}));
+        }));
+
+
     }
 
     private static Function createFunction(String functionName, BiFunction<Arguments, FunctionTable, Variable> function) {
